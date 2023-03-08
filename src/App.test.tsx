b6823078d40f702d12a1a0bc40f905a82a94b9fb
Curtis Turk <curtis.c.turk@outlook.com>
2023-03-08 11:33:00 +0000

    Adding tests for form inputs and labels

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("full name has correct label", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const message: string = "Full Name * :";
+  expect(screen.getByLabelText(message)).toBeInTheDocument();
 });
+test("phone number has correct label", () => {
+  render(<App />);
+  const message: string = "Phone Number * :";
+  expect(screen.getByLabelText(message)).toBeInTheDocument();
+});
+// test("phone number has correct label", () => {
+//   render(<App />);
+//   const message: string = "Phone Number * :";
+//   expect(screen.getByText(message)).toBeInTheDocument();
+// });
+
+// test("fullname form input wrong", () => {
+//   render(<App />);
+//   const message: string = "Fulls Names * :";
+//   // const linkElement = screen.getByText(/learn react/i);
+//   // expect(linkElement).toBeInTheDocument();
+//   expect(screen.getByText(message)).toBeInTheDocument();
+// });
--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,10 +1,11 @@
 export default function Form() {
   return (
     <form id="polling-form">
-      <label>Full Name * :</label>
-      <input type="text" name="fullname" />
-      <label>Phone Number * :</label>
-      <input type="text" name="phone" />
+      <label htmlFor="fullname">Full Name * :</label>
+      <input type="text" id="fullname" />
+      <label htmlFor="phone">Phone Number * :</label>
+      <input type="text" id="phone" />
+
       <label>When would you like a reminder?:</label>
       <p>
         One week before
@@ -14,7 +15,6 @@ export default function Form() {
           id="one-week"
           value="7 days"
         />
-        <br />
         Three days before
         <input
           type="checkbox"
@@ -22,10 +22,8 @@ export default function Form() {
           id="three-days"
           value="3 days"
         />
-        <br />
         On the day
         <input type="radio" name="reminders[]" id="on-the-day" value="0 days" />
-        <br />
       </p>
       <input type="submit" value="click here to submit" />
     </form>
