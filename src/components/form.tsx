c4cc2c44b9a6fcf7e380b7dd5013575fe3f89f31
Harry Fox <49591810+hfox1@users.noreply.github.com>
2023-03-21 12:01:44 +0000

    ggg Form.tsx, Title.tsx capitalised

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,121 +1,121 @@
-import { useState, Dispatch, SetStateAction } from "react";
-import PhoneInput from "react-phone-number-input";
-import { Postcode } from "./Postcode";
+import { useState, Dispatch, SetStateAction } from 'react';
+import PhoneInput from 'react-phone-number-input';
+import { Postcode } from './Postcode';
 
 interface formProps {
-  setIsFormSubmitted: Dispatch<SetStateAction<boolean>>;
+	setIsFormSubmitted: Dispatch<SetStateAction<boolean>>;
 }
 
 export interface formData {
-  name: string;
-  phone: string;
-  postcode: string;
-  messageType: string;
-  addressSlug: string;
+	name: string;
+	phone: string;
+	postcode: string;
+	messageType: string;
+	addressSlug: string;
 }
 
 export const Form = ({ setIsFormSubmitted }: formProps) => {
-  // boolean for if postcode has been verified with the Electoral Commission API
-  const [isPostcodeVerified, setIsPostCodeVerified] = useState(false);
+	// boolean for if postcode has been verified with the Electoral Commission API
+	const [isPostcodeVerified, setIsPostCodeVerified] = useState(false);
 
-  const [formData, setFormData] = useState<formData>({
-    name: "",
-    phone: "",
-    postcode: "",
-    messageType: "WhatsApp",
-    addressSlug: "",
-  });
+	const [formData, setFormData] = useState<formData>({
+		name: '',
+		phone: '',
+		postcode: '',
+		messageType: 'WhatsApp',
+		addressSlug: '',
+	});
 
-  const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const name: string = e.target.name;
-    const value: string = e.target.value;
+	const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const name: string = e.target.name;
+		const value: string = e.target.value;
 
-    setFormData((formData) => ({
-      ...formData,
-      [name]: value,
-    }));
-  };
+		setFormData((formData) => ({
+			...formData,
+			[name]: value,
+		}));
+	};
 
-  const handlePhoneInputChange = (phoneNumber: any) => {
-    setFormData((formData) => ({
-      ...formData,
-      phone: phoneNumber,
-    }));
-  };
+	const handlePhoneInputChange = (phoneNumber: any) => {
+		setFormData((formData) => ({
+			...formData,
+			phone: phoneNumber,
+		}));
+	};
 
-  const handleSubmit = async () => {
-    if (formData.name && formData.phone) {
-      const response = await fetch(
-        `${process.env.REACT_APP_API as string}/submit`,
-        {
-          method: "POST",
-          body: JSON.stringify(formData),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
+	const handleSubmit = async () => {
+		if (formData.name && formData.phone) {
+			const response = await fetch(
+				`${process.env.REACT_APP_API as string}/submit`,
+				{
+					method: 'POST',
+					body: JSON.stringify(formData),
+					headers: {
+						'Content-Type': 'application/json',
+					},
+				}
+			);
 
-      if (response.ok) {
-        await setIsFormSubmitted(true);
-      }
-    }
-    if (!formData.name) {
-      // changes state of name error
-      // change state of error object
-      return;
-    }
-    if (!formData.phone) {
-      return;
-    }
-  };
+			if (response.ok) {
+				await setIsFormSubmitted(true);
+			}
+		}
+		if (!formData.name) {
+			// changes state of name error
+			// change state of error object
+			return;
+		}
+		if (!formData.phone) {
+			return;
+		}
+	};
 
-  return (
-    <div id="polling-form">
-      <div id="user-details">
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" onChange={handleTextChange} />
-        <label htmlFor="phone">Phone Number:</label>
-        <PhoneInput defaultCountry="GB" onChange={handlePhoneInputChange} />
-        <Postcode
-          {...{
-            isPostcodeVerified,
-            setIsPostCodeVerified,
-            postcode: formData.postcode,
-            setFormData,
-            handleTextChange,
-          }}
-        />
-      </div>
+	return (
+		<div id="polling-form">
+			<div id="user-details">
+				<label htmlFor="name">Name:</label>
+				<input type="text" id="name" name="name" onChange={handleTextChange} />
+				<label htmlFor="phone">Phone Number:</label>
+				<PhoneInput defaultCountry="GB" onChange={handlePhoneInputChange} />
+				<Postcode
+					{...{
+						isPostcodeVerified,
+						setIsPostCodeVerified,
+						postcode: formData.postcode,
+						setFormData,
+						handleTextChange,
+					}}
+				/>
+			</div>
 
-      <fieldset id="message-type">
-        <legend>How would you like your reminder?</legend>
-        <span>
-          WhatsApp
-          <input
-            type="radio"
-            name="messageType"
-            id="WhatsApp"
-            value="WhatsApp"
-            defaultChecked={true}
-            onChange={handleTextChange}
-          />
-        </span>
-        <span>
-          SMS
-          <input
-            type="radio"
-            name="messageType"
-            id="Sms"
-            value="Sms"
-            onChange={handleTextChange}
-          />
-        </span>
-      </fieldset>
-      <div>We will send you a reminder on the day of the election</div>
-      <button onClick={handleSubmit} id="submit-form-btn">
-        Submit
-      </button>
-    </div>
-  );
+			<fieldset id="message-type">
+				<legend>How would you like your reminder?</legend>
+				<span>
+					WhatsApp
+					<input
+						type="radio"
+						name="messageType"
+						id="WhatsApp"
+						value="WhatsApp"
+						defaultChecked={true}
+						onChange={handleTextChange}
+					/>
+				</span>
+				<span>
+					SMS
+					<input
+						type="radio"
+						name="messageType"
+						id="Sms"
+						value="Sms"
+						onChange={handleTextChange}
+					/>
+				</span>
+			</fieldset>
+			<div>We will send you a reminder on the day of the election</div>
+			<button onClick={handleSubmit} id="submit-form-btn">
+				Submit
+			</button>
+		</div>
+	);
 };
