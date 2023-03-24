a019c5529b716cffdd8fd0805b5644d4f56a06eb
Curtis Turk <curtis.c.turk@outlook.com>
2023-03-24 15:37:45 +0000

    testing for an address picker response

--- test/api/postcode.test.tsx
+++ test/api/postcode.test.tsx
@@ -2,6 +2,7 @@ import { mockRequestResponse } from "./apiSetup";
 import postcode from "../../pages/api/postcode";
 import axios, { AxiosError } from "axios";
 import pollingDataExistsResponse from "./mockApiResponses/pollingDataExistsResponse";
+import addressPickerResponse from "./mockApiResponses/addressPickerResponse";
 jest.mock("axios");
 const mockedAxiosGet = axios.get as jest.MockedFunction<typeof axios>;
 
@@ -22,6 +23,39 @@ describe("/postcode api route", () => {
     expect(res._getJSONData()).toEqual({
       pollingStationFound: true,
       pollingStations: [],
+    });
+  });
+
+  it("returns an address picker", async () => {
+    mockedAxiosGet.mockResolvedValueOnce({ data: addressPickerResponse });
+    const { req, res } = mockRequestResponse("POST");
+
+    req.headers = {
+      origin: process.env.NEXT_PUBLIC_API,
+    };
+
+    const postcodeRequest = { postcode: "TN4TWH" };
+    req.body = postcodeRequest;
+
+    await postcode(req, res);
+    expect(mockedAxiosGet).toHaveBeenCalledWith(
+      `https://api.electoralcommission.org.uk/api/v1/postcode/TN4TWH?token=${process.env.EC_API_KEY}`
+    );
+    expect(res.statusCode).toBe(200);
+    expect(res._getJSONData()).toEqual({
+      pollingStationFound: false,
+      pollingStations: [
+        {
+          address: "16 DUNCAN CLOSE, ST. MELLONS, CARDIFF",
+          postcode: "CF3 1NP",
+          slug: "100100106448",
+        },
+        {
+          address: "26 DUNCAN CLOSE, ST. MELLONS, CARDIFF",
+          postcode: "CF3 1NP",
+          slug: "100100106458",
+        },
+      ],
     });
   });
 });
