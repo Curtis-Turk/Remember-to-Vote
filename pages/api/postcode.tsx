9556e9541be24f9520cc01bc5f8030290ae07550
Curtis Turk <curtis.c.turk@outlook.com>
2023-03-27 16:08:01 +0100

    added a return to postcode route

--- pages/api/postcode.tsx
+++ pages/api/postcode.tsx
@@ -9,11 +9,10 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   // if (req.headers.origin !== process.env.NEXT_PUBLIC_API) {
   //   return res.status(401);
   // }
-
   // const pollingStationResponse = await electoralCommission.verifyPostcode(
   //   req.body.postcode
   // );
   // const statusCode = pollingStationResponse.errorMessage ? 400 : 200;
   // return res.status(statusCode).json(pollingStationResponse);
-  return res.status(200);
+  return res.status(204).send("Hello world");
 };
