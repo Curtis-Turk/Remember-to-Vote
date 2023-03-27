6160a84dc1a3bd56d0e5c0b8582aa9f37a75148d
Curtis Turk <curtis.c.turk@outlook.com>
2023-03-27 16:13:39 +0100

    removed origin check

--- pages/api/postcode.tsx
+++ pages/api/postcode.tsx
@@ -6,13 +6,15 @@ const electoralCommission = new ElectoralCommisionApi(
 );
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.headers.origin !== process.env.NEXT_PUBLIC_API) {
-    return res.status(401);
-  }
+  // console.log(req.headers);
+  // if (req.headers.origin !== process.env.NEXT_PUBLIC_API) {
+  //   return res.status(401);
+  // }
+  // if (!req.body) return res.status(205);
   const pollingStationResponse = await electoralCommission.verifyPostcode(
     req.body.postcode
   );
   const statusCode = pollingStationResponse.errorMessage ? 400 : 200;
-  // return res.status(statusCode).json(pollingStationResponse);
-  return res.status(204).send("Hello world");
+  return res.status(statusCode).json(pollingStationResponse);
+  // return res.status(200).send("Hello world");
 };
