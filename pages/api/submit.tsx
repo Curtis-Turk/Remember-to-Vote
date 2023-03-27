882f1073fcee2292a8a56c03e658bf25d062c499
Curtis Turk <curtis.c.turk@outlook.com>
2023-03-27 14:45:14 +0100

    Added a sumbit path to the api

--- pages/api/submit.tsx
+++ pages/api/submit.tsx
@@ -5,4 +5,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.headers.origin !== process.env.NEXT_PUBLIC_API) {
     return res.status(401);
   }
+
+  await supabase.from("voters");
 };
--- test/api/submit.test.tsx
+++ test/api/submit.test.tsx
@@ -1,7 +1,15 @@
 import { mockRequestResponse } from "./apiSetup";
 import submit from "../../pages/api/submit";
-import supabase from "../../utils/supabase";
-jest.mock("../../utils/supabase");
+// import supabase from "../../utils/supabase";
+jest.mock("../../utils/supabase", () => ({
+  __esModule: true,
+  superbase: {
+    from: jest.fn(),
+    insert: jest.fn(),
+  },
+}));
+const superbaseFrom = jest.requireMock("../../utils/supabase").superbase.from;
+// const mockedSupabaseFrom = supabase.from as jest.MockedFunction<typeof supabase>;
 
 describe("/submit api route", () => {
   it("returns 401 if not the same origin", async () => {
@@ -15,5 +23,21 @@ describe("/submit api route", () => {
     };
     await submit(req, res);
     expect(res.statusCode).toBe(401);
+  });
+
+  it("successfully submits a form to the db", async () => {
+    const { req, res } = mockRequestResponse("POST");
+    req.headers = {
+      origin: process.env.NEXT_PUBLIC_API,
+    };
+    req.body = {
+      name: "Curtis Turk",
+      phone: "+447777777777",
+      postcode: "ST7 2AE",
+      messageType: "WhatsApp",
+      addressSlug: "",
+    };
+    await submit(req, res);
+    expect(superbaseFrom).toHaveBeenCalledWith("voters");
   });
 });
