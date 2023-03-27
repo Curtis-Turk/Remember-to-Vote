2f08f2c1b7b7359f545b16e52fab88760bfa72cc
Curtis Turk <curtis.c.turk@outlook.com>
2023-03-27 16:09:19 +0100

    uncommented EC logic

--- pages/api/postcode.tsx
+++ pages/api/postcode.tsx
@@ -1,18 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
-// import ElectoralCommisionApi from "../../lib/electoralCommisionApi";
+import ElectoralCommisionApi from "../../lib/electoralCommisionApi";
 
-// const electoralCommission = new ElectoralCommisionApi(
-//   process.env.EC_API_KEY as string
-// );
+const electoralCommission = new ElectoralCommisionApi(
+  process.env.EC_API_KEY as string
+);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  // if (req.headers.origin !== process.env.NEXT_PUBLIC_API) {
-  //   return res.status(401);
-  // }
-  // const pollingStationResponse = await electoralCommission.verifyPostcode(
-  //   req.body.postcode
-  // );
-  // const statusCode = pollingStationResponse.errorMessage ? 400 : 200;
+  if (req.headers.origin !== process.env.NEXT_PUBLIC_API) {
+    return res.status(401);
+  }
+  const pollingStationResponse = await electoralCommission.verifyPostcode(
+    req.body.postcode
+  );
+  const statusCode = pollingStationResponse.errorMessage ? 400 : 200;
   // return res.status(statusCode).json(pollingStationResponse);
   return res.status(204).send("Hello world");
 };
