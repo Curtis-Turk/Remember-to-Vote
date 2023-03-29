b493aa9a7ac9c38a5ae1411d9abbb89dd4853a7f
Harry Fox <49591810+hfox1@users.noreply.github.com>
2023-03-29 12:39:44 +0100

    gdpr text added to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,6 @@
 export default function Footer() {
   return (
     <div id="footer">
-      {/* <p>
-        If you submit this information you agree to be contacted on the day of
-        the election, your data will not be used for any other purposes
-      </p> */}
       <p>
         Made for <a href="https://www.campaignlab.uk/">Campaign lab</a>
       </p>
@@ -13,6 +9,15 @@ export default function Footer() {
         <a href="https://democracyclub.org.uk/"> Democracy Club. </a>
       </p>
       <p>Curtis, Zac and Harry</p>
+      <p id="gdpr-text">
+        You can manage your preferences or unsubscribe at any time. We may use the information you
+        provide, such as name and postcode, to match the data provided to your electoral register
+        record held on our electoral database, which could inform future communications you receive
+        from us. We may use your answers to help us campaign better. If signing up for an event, we
+        may still use the details provided to contact you specifically about that event. If you
+        would like to know more about how we use your information click{' '}
+        <a href="https://labour.org.uk/privacy-policy">here</a>.
+      </p>
     </div>
   );
 }
