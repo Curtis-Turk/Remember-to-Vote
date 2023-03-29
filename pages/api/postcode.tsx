33580ce96d73ed60c1b2a79407df699b8a128028
Harry Fox <49591810+hfox1@users.noreply.github.com>
2023-03-29 16:31:09 +0100

    removing wrapper function

--- pages/api/postcode.tsx
+++ pages/api/postcode.tsx
@@ -3,28 +3,7 @@ import ElectoralCommisionApi from '../../lib/electoralCommisionApi';
 
 const electoralCommission = new ElectoralCommisionApi(process.env.EC_API_KEY as string);
 
-const allowCors =
-  (fn: (req: NextApiRequest, res: NextApiResponse) => Promise<void>) =>
-  async (req: NextApiRequest, res: NextApiResponse) => {
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', `${req.headers.origin}`);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    );
-    res.setHeader('Cache-Control', 'no-cache');
-
-    if (req.method === 'OPTIONS') {
-      res.status(200).end();
-      return;
-    }
-    return await fn(req, res);
-  };
-
-export const postcode = async (req: NextApiRequest, res: NextApiResponse) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
   // if (req.headers.origin !== process.env.NEXT_PUBLIC_API) {
   //   return res.status(401);
   // }
@@ -45,5 +24,3 @@ export const postcode = async (req: NextApiRequest, res: NextApiResponse) => {
   // const statusCode = pollingStationResponse.errorMessage ? 400 : 200;
   return res.status(statusCode).json(pollingStationResponse);
 };
-
-module.exports = allowCors(postcode);
