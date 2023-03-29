9e74c0552e0a2331b5870e74cc11d6007ec359ba
Harry Fox <49591810+hfox1@users.noreply.github.com>
2023-03-29 15:30:45 +0100

    trying * in CORS origin

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
         source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: 'remembertovote.org.uk' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
           { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
           {
             key: 'Access-Control-Allow-Headers',
