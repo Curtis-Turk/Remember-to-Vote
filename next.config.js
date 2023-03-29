f135e6568ff24961a48b3e78fd3d4b712d7de6b8
Harry Fox <49591810+hfox1@users.noreply.github.com>
2023-03-29 16:47:08 +0100

    vercel.json and next.config allow-private-network set true

--- next.config.js
+++ next.config.js
@@ -16,6 +16,7 @@ module.exports = {
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
           { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          { key: 'Access-Control-Allow-Private-Network', value: 'true' },
           {
             key: 'Access-Control-Allow-Headers',
             value:
