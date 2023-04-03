94d1a12e4aa71647ed4e038112c965ceed1ab45b
Curtis Turk <curtis.c.turk@outlook.com>
2023-04-03 12:49:17 +0100

    Removed demo text from title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,14 +2,7 @@ export default function Title() {
   return (
     <div id="title-card">
       <h1>Polling Station Reminder 🗳️</h1>
-      {/* <h2>
-        Enter your details to receive a message with your polling station
-        details
-      </h2> */}
-      <h2>
-        Enter your details to receive a demo message with some example polling
-        station details
-      </h2>
+      <h2>Enter your details to receive a message with your polling station details</h2>
     </div>
   );
 }
