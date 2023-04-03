4510753509a36c5b58673b61d8ba992bdc113da0
Curtis Turk <curtis.c.turk@outlook.com>
2023-04-03 14:12:16 +0100

    Small styling edits to title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,8 +1,11 @@
 export default function Title() {
   return (
     <div id="title-card">
-      <h1>Polling Station Reminder 🗳️</h1>
-      <h2>Enter your details to receive a message with your polling station details</h2>
+      <h1>🗳️ Remember To Vote 🗳️ </h1>
+      <p>
+        Enter your details to receive a reminder message on the day of the election with your
+        polling station information
+      </p>
     </div>
   );
 }
