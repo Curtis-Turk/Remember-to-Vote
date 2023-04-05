ab9ca69eae5dc8b0d4589d511ef6d5134341585c
Zac <zac@zacmoss.co.uk>
2023-04-05 14:58:38 +0100

    changed interface to voterTableRow

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -5,7 +5,7 @@ const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 );
 
-export interface voterTable {
+export interface voterTableRow {
   name: string;
   phone_number: string;
   postcode: string;
@@ -24,7 +24,7 @@ returns the object:
   count: number of data objects | null if no number
 }
 */
-export const submitToVotersTable = async (voterTable: voterTable) => {
-  const response = await supabase.from('voters').insert([voterTable]);
+export const submitToVotersTable = async (voterTableRow: voterTableRow) => {
+  const response = await supabase.from('voters').insert([voterTableRow]);
   return response;
 };
