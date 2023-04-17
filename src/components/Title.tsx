3c228aebb372c79776ed9fe93a990f3d043dcf7a
Zac <zac@zacmoss.co.uk>
2023-04-17 20:34:45 +0100

    centred stack with container

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,20 +1,22 @@
-import { Button, Stack } from 'react-bootstrap';
+import { Button, Container, Stack } from 'react-bootstrap';
 import About from './About';
 
 export default function Title() {
   return (
     <div id="title-card">
-      <Stack direction="horizontal" style={{ gap: '89px' }}>
-        <Stack>
-          <h1 id="main-title">
-            Remember
-            <br />
-            To <span id="vote-title">Vote</span>
-          </h1>
-          <Button variant="register">Register for your reminder</Button>
+      <Container>
+        <Stack direction="horizontal" style={{ gap: '89px' }}>
+          <Stack>
+            <h1 id="main-title">
+              Remember
+              <br />
+              To <span id="vote-title">Vote</span>
+            </h1>
+            <Button variant="register">Register for your reminder</Button>
+          </Stack>
+          <About />
         </Stack>
-        <About />
-      </Stack>
+      </Container>
     </div>
   );
 }
