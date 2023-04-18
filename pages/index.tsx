7a766c32e5559826a4a0a87580b7f29e2ef5285d
Zac <zac@zacmoss.co.uk>
2023-04-18 13:20:24 +0100

    created index.tsx and moved styles import to _app

---
+++ pages/index.tsx
@@ -0,0 +1,27 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect } from 'react';
+import { Inter } from 'next/font/google';
+
+import Title from '../src/components/Title';
+import Footer from '../src/components/Footer';
+import FormSection from '../src/components/FormSection';
+
+const inter = Inter({
+  weight: ['400', '600', '700'],
+  subsets: ['latin'],
+});
+
+function App() {
+  useEffect(() => {
+    const bootstrap = require('bootstrap');
+  }, []);
+
+  return (
+    <div className={inter.className} id="App">
+      <Title />
+      <FormSection />
+      <Footer />
+    </div>
+  );
+}
+export default App;
