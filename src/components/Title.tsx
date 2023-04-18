e1dcb28c1d9c9253a5368972f28144a029b050a1
Curtis Turk <curtis.c.turk@outlook.com>
2023-04-18 10:46:45 +0100

    Added ballot Icon to Title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,12 +1,16 @@
 import { Button, Container, Stack } from 'react-bootstrap';
 import About from './About';
 import Link from 'next/link';
+import Image from 'next/image';
+import ballotIcon from '../../assets/icon.png';
+
 export default function Title() {
   return (
     <div id="title-card">
       <Container>
         <Stack direction="horizontal" style={{ gap: '89px' }}>
           <Stack>
+            <Image src={ballotIcon} alt="ballot-icon"></Image>
             <h1 id="main-title">
               Remember
               <br />
