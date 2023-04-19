4754ec7109b4e884a9ec8e43edc1dd1dd6ec8dee
Zac <zac@zacmoss.co.uk>
2023-04-19 15:54:39 +0100

    added mobile breakpoint for title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -7,34 +7,61 @@ import pinkOval from '../../assets/pink oval line.svg';
 import ballotBox from '../../assets/Ballot box.svg';
 import headerEllipse from '../../assets/header.svg';
 import downArrow from '../../assets/down-arrow.svg';
+import useMediaQuery from '../../lib/useMediaQuery';
 
 export default function Title() {
+  const isMobileBreakpoint = useMediaQuery(600);
+  const renderStack = () => {
+    if (isMobileBreakpoint) {
+      return (
+        <Stack>
+          <Image src={ballotIcon} alt="ballot-icon"></Image>
+          <h1 id="main-title">
+            Remember
+            <br />
+            to&nbsp;<Image src={pinkOval} alt="vote-circle" id="pink-oval"></Image>
+            <span id="vote-title">Vote</span>
+          </h1>
+          <Link href="#form-section" scroll={false}>
+            <Button variant="register">Register for your reminder</Button>
+          </Link>
+          <About />
+          <Link href="#form-section" scroll={false}>
+            <Image src={downArrow} alt="down-arrow"></Image>
+          </Link>
+          <Image src={ballotBox} alt="ballot-box" id="ballot-box"></Image>
+        </Stack>
+      );
+    }
+    return (
+      <Stack direction="horizontal" id="title-stack">
+        <Stack>
+          <Image src={ballotIcon} alt="ballot-icon"></Image>
+          <h1 id="main-title">
+            Remember
+            <br />
+            to&nbsp;<Image src={pinkOval} alt="vote-circle" id="pink-oval"></Image>
+            <span id="vote-title">Vote</span>
+          </h1>
+          <Link href="#form-section" scroll={false}>
+            <Button variant="register">Register for your reminder</Button>
+          </Link>
+          <Image src={ballotBox} alt="ballot-box" id="ballot-box"></Image>
+        </Stack>
+        <Stack>
+          <About />
+          <Link href="#form-section" scroll={false}>
+            <Image src={downArrow} alt="down-arrow"></Image>
+          </Link>
+        </Stack>
+      </Stack>
+    );
+  };
+
   return (
     <div id="title-card">
       <Image src={headerEllipse} alt="header-ellipse" id="header-ellipse"></Image>
-      <Container id="title-container">
-        <Stack direction="horizontal" id="title-stack">
-          <Stack>
-            <Image src={ballotIcon} alt="ballot-icon"></Image>
-            <h1 id="main-title">
-              Remember
-              <br />
-              to&nbsp;<Image src={pinkOval} alt="vote-circle" id="pink-oval"></Image>
-              <span id="vote-title">Vote</span>
-            </h1>
-            <Link href="#form-section" scroll={false}>
-              <Button variant="register">Register for your reminder</Button>
-            </Link>
-            <Image src={ballotBox} alt="ballot-box" id="ballot-box"></Image>
-          </Stack>
-          <Stack>
-            <About />
-            <Link href="#form-section" scroll={false}>
-              <Image src={downArrow} alt="down-arrow"></Image>
-            </Link>
-          </Stack>
-        </Stack>
-      </Container>
+      <Container id="title-container">{renderStack()}</Container>
     </div>
   );
 }
