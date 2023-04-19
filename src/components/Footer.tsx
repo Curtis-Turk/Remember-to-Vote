08f233bde95525582d0a18ae3a3e8b63d4a0d6bf
Zac <zac@zacmoss.co.uk>
2023-04-19 14:23:20 +0100

    added repeating background to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,28 +5,30 @@ import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <div id="footer">
-      <Image src={ballotIcon} alt="ballot-icon"></Image>
-      <p>
-        Made for <a href="https://www.campaignlab.uk/">Campaign lab</a>
-      </p>
-      <p>
-        Data provided in part by
-        <a href="https://democracyclub.org.uk/"> Democracy Club. </a>
-      </p>
-      <p>Created by Curtis, Harry, Joe and Zac</p>
-      <p id="gdpr-text">
-        You can manage your preferences or unsubscribe at any time. We may use the information you
-        provide, such as name and postcode, to match the data provided to your electoral register
-        record held on our electoral database, which could inform future communications you receive
-        from us. We may use your answers to help us campaign better. If signing up for an event, we
-        may still use the details provided to contact you specifically about that event. If you
-        would like to know more about how we use your information click{' '}
-        <a href="https://labour.org.uk/privacy-policy">here</a>.
-      </p>
-      <Link href="#title-card" scroll={false}>
-        <Image src={downArrow} alt="up-arrow" id="up-arrow"></Image>
-      </Link>
+    <div id="footer-background">
+      <div id="footer">
+        <Image src={ballotIcon} alt="ballot-icon"></Image>
+        <p>
+          Made for <a href="https://www.campaignlab.uk/">Campaign lab</a>
+        </p>
+        <p>
+          Data provided in part by
+          <a href="https://democracyclub.org.uk/"> Democracy Club. </a>
+        </p>
+        <p>Created by Curtis, Harry, Joe and Zac</p>
+        <p id="gdpr-text">
+          You can manage your preferences or unsubscribe at any time. We may use the information you
+          provide, such as name and postcode, to match the data provided to your electoral register
+          record held on our electoral database, which could inform future communications you
+          receive from us. We may use your answers to help us campaign better. If signing up for an
+          event, we may still use the details provided to contact you specifically about that event.
+          If you would like to know more about how we use your information click{' '}
+          <a href="https://labour.org.uk/privacy-policy">here</a>.
+        </p>
+        <Link href="#title-card" scroll={false}>
+          <Image src={downArrow} alt="up-arrow" id="up-arrow"></Image>
+        </Link>
+      </div>
     </div>
   );
 }
