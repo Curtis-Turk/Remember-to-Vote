fa0e7dd6340510009085cd90b3702df30deca443
Zac <zac@zacmoss.co.uk>
2023-04-20 18:07:20 +0100

    submitted page updated

--- src/components/SubmittedForm.tsx
+++ src/components/SubmittedForm.tsx
@@ -1,31 +1,29 @@
 import { SetStateAction, Dispatch } from 'react';
+import { Alert, Button } from 'react-bootstrap';
 
 interface formProps {
+  // TODO: may delete this eventually.
   setIsFormSubmitted: Dispatch<SetStateAction<boolean>>;
 }
 
+// Waiting on Joe's design thoughts.
 export default function SubmittedForm({ setIsFormSubmitted }: formProps) {
   return (
-    <div id="polling-form">
-      <h2>Submitted</h2>
+    <Alert variant="success">
+      <Alert.Heading>Submitted</Alert.Heading>
+      <br />
       <p>
-        You will receive a message on the day of the election with your polling station information
+        You will receive a message on the day of the election with your polling station information.
       </p>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
+      {/* <hr />
+      <Button
+        variant="success"
+        // id="submit-form-btn"
+        // className="submitEnabled"
+        onClick={() => setIsFormSubmitted(false)}
       >
-        <button
-          id="submit-form-btn"
-          className="submitEnabled"
-          onClick={() => setIsFormSubmitted(false)}
-        >
-          Back to main page
-        </button>
-      </div>
-    </div>
+        Back to main page
+      </Button> */}
+    </Alert>
   );
 }
