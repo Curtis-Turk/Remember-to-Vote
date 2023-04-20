3431d898a96f28811c61bb44f65f5e246f7b6d88
Zac <zac@zacmoss.co.uk>
2023-04-20 11:45:59 +0100

    added types to CancelButton

--- src/components/CancelButton.tsx
+++ src/components/CancelButton.tsx
@@ -1,20 +1,35 @@
+import { Dispatch, SetStateAction } from 'react';
 import { Button } from 'react-bootstrap';
+import { addressObject, setStateAddresses, setStateSelectedAddress } from './Postcode';
+
+type setStateBoolean = Dispatch<SetStateAction<boolean>>;
+
+interface cancelButtonProps {
+  setIsCancelButtonRendered: setStateBoolean;
+  isCancelButtonRendered: boolean;
+  setIsVerifyPostcodeDisabled: setStateBoolean;
+  setIsVerifyPostcodeButtonRendered: setStateBoolean;
+  setIsPostcodeVerified: setStateBoolean;
+  setSelectedAddress: setStateSelectedAddress;
+  setAddresses: setStateAddresses;
+  defaultAddressObject: addressObject;
+}
 
 export default function CancelButton({
   setIsCancelButtonRendered,
   isCancelButtonRendered,
   setIsVerifyPostcodeDisabled,
   setIsVerifyPostcodeButtonRendered,
-  setIsPostCodeVerified,
+  setIsPostcodeVerified,
   setSelectedAddress,
   setAddresses,
   defaultAddressObject,
-}) {
+}: cancelButtonProps) {
   if (isCancelButtonRendered) {
     const handleCancelButtonClick = (): void => {
       setIsVerifyPostcodeDisabled(false);
       setIsVerifyPostcodeButtonRendered(true);
-      setIsPostCodeVerified(false);
+      setIsPostcodeVerified(false);
       setIsCancelButtonRendered(false);
       setSelectedAddress(defaultAddressObject);
       setAddresses([]);
