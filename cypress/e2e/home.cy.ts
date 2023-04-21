0db763b372e5f0b9f1bf04b910b41fc6f0b475af
Harry Fox <49591810+hfox1@users.noreply.github.com>
2023-04-21 13:21:05 +0100

    address picker cypress test addded

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -8,11 +8,19 @@ describe('visit homepage', () => {
   });
 
   it('Can verify postcode', () => {
-    cy.get('#name').type('Curtis Turk');
-    cy.get('.PhoneInputInput').type('07436468661');
+    // cy.get('#name').type('Curtis Turk');
+    // cy.get('.PhoneInputInput').type('07436468661');
     cy.get('#postcode').type('ST7 2AE');
     const verifyBtn = cy.get('#verify-btn');
     verifyBtn.click();
     verifyBtn.contains('Postcode verified!');
+  });
+
+  it('Can select an address from the address picker', () => {
+    cy.get('#postcode').type('ST7 2AF');
+    const verifyBtn = cy.get('#verify-btn');
+    verifyBtn.click();
+    verifyBtn.contains('Postcode verified!');
+    cy.get('.form-select').select('6 LAWTON ROAD, ALSAGER, STOKE-ON-TRENT');
   });
 });
