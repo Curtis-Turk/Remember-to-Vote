835f68bb124b44ebb2d7eece6a294d5460b7f1ee
Harry Fox <49591810+hfox1@users.noreply.github.com>
2023-04-26 14:39:16 +0100

    cors permission added for separate domains

--- pages/api/fire/[key].tsx
+++ pages/api/fire/[key].tsx
@@ -1,7 +1,16 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import Cors from 'cors';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+const cors = Cors({
+  origin: '*',
+  methods: ['GET'],
+});
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  await cors(req, res);
+
   const { key } = req.query;
+
   if (key === 'harryfox') {
     res.status(200).send('SHOTS FIRED! With key ' + key);
   } else res.status(401).send('key does not match\n' + key);
