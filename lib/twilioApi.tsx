1c1635c00c51aa36a9fd3680f4f373c20482959b
Harry Fox <49591810+hfox1@users.noreply.github.com>
2023-04-26 17:21:57 +0100

    twilio true e2e testing route added

--- lib/twilioApi.tsx
+++ lib/twilioApi.tsx
@@ -4,6 +4,7 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID as string;
 const authToken = process.env.TWILIO_AUTH_TOKEN as string;
 const fromNumberWhatsapp = process.env.TWILIO_FROM_NUMBER_WHATSAPP as string;
 const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID as string;
+const testingServiceSid = process.env.TWILIO_TESTING_SERVICE_SID as string;
 const client = new Twilio(accountSid, authToken);
 
 interface createParams {
@@ -14,6 +15,11 @@ interface createParams {
 }
 
 const sendMessage = async (createParams: createParams): Promise<boolean> => {
+  //catch testing
+  if (createParams.to === '+15005550006') {
+    createParams.messagingServiceSid = testingServiceSid;
+  }
+  // send message
   try {
     const message = await client.messages.create(createParams);
     console.log(`Message sent! SID: ${message.sid}`);
