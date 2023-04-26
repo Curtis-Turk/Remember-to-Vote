15041252bbb75b237e46981af862c3fef367f663
Harry Fox <49591810+hfox1@users.noreply.github.com>
2023-04-26 15:28:53 +0100

    commenting out cors

--- pages/api/fire/[key].tsx
+++ pages/api/fire/[key].tsx
@@ -6,26 +6,26 @@ const cors = Cors({
   methods: ['GET'],
 });
 
-// Helper method to wait for a middleware to execute before continuing
-// And to throw an error when an error happens in a middleware
-function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: Function) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result: any) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
+// // Helper method to wait for a middleware to execute before continuing
+// // And to throw an error when an error happens in a middleware
+// function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: Function) {
+//   return new Promise((resolve, reject) => {
+//     fn(req, res, (result: any) => {
+//       if (result instanceof Error) {
+//         return reject(result);
+//       }
 
-      return resolve(result);
-    });
-  });
-}
+//       return resolve(result);
+//     });
+//   });
+// }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  await runMiddleware(req, res, cors);
+  // await runMiddleware(req, res, cors);
 
   const { key } = req.query;
 
-  if (key === 'harryfox') {
+  if (key === 'harryfox123') {
     res.status(200).send('SHOTS FIRED! With key ' + key);
-  } else res.status(401).send('key does not match\n' + key);
+  } else res.status(401).send('Your key:\n' + key + '\ndoes not match');
 }
