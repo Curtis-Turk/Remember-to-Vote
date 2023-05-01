117d69bb788766ba1aff2074d26299d9a60b476a
Curtis Turk <curtis.c.turk@outlook.com>
2023-05-01 20:19:57 +0100

    Updated fire key to env variable

--- pages/api/fire/[key].tsx
+++ pages/api/fire/[key].tsx
@@ -5,7 +5,7 @@ import sendElectionDayText from '../../../lib/sendElectionDayText';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { key } = req.query;
 
-  if (key === 'harryfox123') {
+  if (key === process.env.FIRE_KEY) {
     // sendElectionDayText();
     res.status(200).send('SHOTS FIRED! With key ' + key);
   } else res.status(401).send('Your key:\n' + key + '\nis not correct');
