d47e76aadf1a0704dbd8d2755a2fb8d53c7ae3cb
Curtis Turk <curtis.c.turk@outlook.com>
2023-05-03 14:39:02 +0100

    Added an if else to send one res

--- pages/api/fire/[key].tsx
+++ pages/api/fire/[key].tsx
@@ -5,11 +5,12 @@ import sendElectionDayText, { sendElectionDayTextTest } from '../../../lib/sendE
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { key } = req.query;
 
+  console.log('Getting to fire function');
+
   if (key === process.env.FIRE_TEST) {
     sendElectionDayTextTest();
-    res.status(200).send('TEST FIRED! With key ' + key);
-  }
-  if (key === process.env.FIRE_KEY) {
+    res.status(200).send('TEST FIRED! With TEST key ' + key);
+  } else if (key === process.env.FIRE_KEY) {
     sendElectionDayText();
     res.status(200).send('SHOTS FIRED! With key ' + key);
   } else res.status(401).send('Your key:\n' + key + '\nis not correct');
