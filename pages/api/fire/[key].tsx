e3879fa506eb0aa95d7aab26f45633235ee01f47
Curtis Turk <curtis.c.turk@outlook.com>
2023-05-03 14:14:20 +0100

    removed extra fire

--- pages/api/fire/[key].tsx
+++ pages/api/fire/[key].tsx
@@ -9,8 +9,4 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     sendElectionDayTextTest();
     res.status(200).send('TEST FIRED! With key ' + key);
   }
-  if (key === process.env.FIRE_KEY) {
-    sendElectionDayText();
-    res.status(200).send('SHOTS FIRED! With key ' + key);
-  } else res.status(401).send('Your key:\n' + key + '\nis not correct');
 }
